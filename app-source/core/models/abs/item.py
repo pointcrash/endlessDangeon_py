a0daee79459786d892de5dcba295199741734aca
pointcrash <r.s.ricce@gmail.com>
2025-02-21 12:15:34 +0100

    create item abs model

---
+++ app-source/core/models/abs/item.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Text
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.models.base import Base
+from core.models.mixins.int_id_pk import IntIdPkMixin
+
+
+class Item(IntIdPkMixin, Base):
+    __abstract__ = True
+
+    name: Mapped[str]
+    description: Mapped[str] = mapped_column(Text, default="", server_default="")
+    type: Mapped[str]
+    cost: Mapped[int] = mapped_column(default=1, server_default="1")
+    image: Mapped[str]
