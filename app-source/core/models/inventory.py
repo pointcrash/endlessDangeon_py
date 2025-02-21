b4db1eef895c36d1b84b5dd5a612050a88b54207
pointcrash <r.s.ricce@gmail.com>
2025-02-21 16:51:27 +0100

    add default values for inventory model

--- app-source/core/models/inventory.py
+++ app-source/core/models/inventory.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models.base import Base
 from .mixins.int_id_pk import IntIdPkMixin
@@ -9,5 +9,5 @@ class Inventory(IntIdPkMixin, PlayerCharacterRelationMixin, Base):
     _character_id_unique = True
     _character_back_populates = "inventory"
 
-    slots_count: Mapped[int]
-    gold: Mapped[int]
+    slots_count: Mapped[int] = mapped_column(default=10, server_default="10")
+    gold: Mapped[int] = mapped_column(default=100, server_default="100")
