301abbbaf1c5ab64d97737745a712412a5a02f15
pointcrash <r.s.ricce@gmail.com>
2025-02-21 12:49:58 +0100

    add model inventory

---
+++ app-source/core/models/inventory.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped
+
+from core.models.base import Base
+from .mixins.int_id_pk import IntIdPkMixin
+from .mixins.player_char_rel import PlayerCharacterRelationMixin
+
+
+class CharacterEquipment(IntIdPkMixin, PlayerCharacterRelationMixin, Base):
+    _character_id_unique = True
+    _character_back_populates = "inventory"
+
+    slots_count: Mapped[int]
+    gold: Mapped[int]
