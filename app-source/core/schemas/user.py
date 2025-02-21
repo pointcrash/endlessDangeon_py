5c2153011c1ebee3cf105e63d3d97d0c09f5d8fc
pointcrash <r.s.ricce@gmail.com>
2025-02-21 14:13:32 +0100

    add schemas for users

--- app-source/core/schemas/user.py
+++ app-source/core/schemas/user.py
@@ -11,5 +11,15 @@ class UserCreate(UserBase):
     pass
 
 
+class UserUpdate(UserBase):
+    pass
+
+
+class UserUpdatePartial(UserBase):
+    username: str | None = None
+    telegram_id: int | None = None
+    language: str | None = None
+
+
 class UserRead(UserBase):
     id: int
