d6bc83e831d9baf72c3a64efccf51ae68308f82e
pointcrash <r.s.ricce@gmail.com>
2025-03-01 22:41:31 +0100

    change regular crud for character to universal model

--- app-source/api/api_v1/crud/characters.py
+++ app-source/api/api_v1/crud/characters.py
@@ -1,10 +1,3 @@
-from typing import Sequence
-
-from sqlalchemy import select
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from core.models import PlayerCharacter
 from core.schemas.character import (
     PlayerCharacterCreate,
     PlayerCharacterUpdate,
@@ -12,52 +5,19 @@ from core.schemas.character import (
 )
 
 
-async def get_all_characters(
-    session: AsyncSession,
-) -> Sequence[PlayerCharacter]:
-    stmt = select(PlayerCharacter).order_by(PlayerCharacter.id)
-    result = await session.scalars(stmt)
-    return result.all()
+from api.api_v1.crud import AsyncCRUDBase
+from core.models import PlayerCharacter
 
 
-async def get_character_by_id(
-    session: AsyncSession, character_id: int
-) -> PlayerCharacter | None:
-    stmt = select(PlayerCharacter).where(PlayerCharacter.id == character_id)
-    result = await session.execute(stmt)
-    character = result.scalar_one_or_none()
-    return character
+class AsyncCRUDCharacter(
+    AsyncCRUDBase[
+        PlayerCharacter,
+        PlayerCharacterCreate,
+        PlayerCharacterUpdate,
+        PlayerCharacterPartialUpdate,
+    ]
+):
+    pass
 
 
-async def create_character(
-    character_create: PlayerCharacterCreate,
-    session: AsyncSession,
-) -> PlayerCharacter:
-    character = PlayerCharacter(**character_create.model_dump())
-    session.add(character)
-    try:
-        await session.commit()
-        return character
-    except IntegrityError:
-        await session.rollback()
-        raise
-
-
-async def update_character(
-    session: AsyncSession,
-    character: PlayerCharacter,
-    character_update: PlayerCharacterUpdate | PlayerCharacterPartialUpdate,
-    partial: bool = False,
-) -> PlayerCharacter:
-    for name, value in character_update.model_dump(exclude_unset=partial).items():
-        setattr(character, name, value)
-    await session.commit()
-    return character
-
-
-async def delete_character(
-    session: AsyncSession,
-    character: PlayerCharacter,
-) -> None:
-    await session.delete(character)
-    await session.commit()
+characters_crud = AsyncCRUDCharacter(PlayerCharacter)
