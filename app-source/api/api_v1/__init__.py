14afb7073e8f377965f57817725dfe75a7efe181
pointcrash <r.s.ricce@gmail.com>
2025-03-01 22:41:56 +0100

    clearing old files

--- app-source/api/api_v1/__init__.py
+++ app-source/api/api_v1/__init__.py
@@ -3,7 +3,8 @@ from fastapi import APIRouter
 from core.config import settings
 
 from .users import router as users_router
-from .characters import router as characters_router
+from .character_base import router as characters_router
+
 from .inventorys import router as inventory_router
 
 router = APIRouter(prefix=settings.api.v1.prefix)
